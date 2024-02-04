9dd8262e4d99feceb656b1ffaafe34d7ca91ad35
Elias Kelta <eliaskelta1@gmail.com>
2024-02-04 11:48:42 +0100

    added animations

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let img = document.getElementById('img');
+let wrapper = document.getElementById('wrapper');
 
 function roll() {
     console.log('Generating...');
     let i = Math.floor(Math.random() * 9) + 0;
     console.log(i);
-    img.setAttribute('src', 'assets/' + i + '.jpeg');
-    img.setAttribute('alt', i);
+
+    img.style.opacity = 0;
+
+    setTimeout(() => {
+        img.setAttribute('src', 'assets/' + i + '.jpeg');
+        img.setAttribute('alt', i);
+
+        img.style.opacity = 1;
+    }, 500);
 }
